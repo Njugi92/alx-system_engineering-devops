eb3113240312121c9eec4170b50131eaa74ebd73
Njugi92 <kenkaruma.kk@gmail.com>
2023-11-08 00:29:09 +0300

    number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The modules for how many subs"""
+
+
+
+def number_of_subscribers(subreddit):
+    """ Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
+    import requests
+
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
